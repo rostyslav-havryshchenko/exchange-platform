0a41fe494e2afab354418247aae6b4e08e02ee16
Ros <rostik.gavrk@gmail.com>
2025-07-10 22:37:39 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',   
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd'],
+          recharts: ['recharts'],
+          router: ['react-router-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500
+  }
 })
